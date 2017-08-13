61013d8f86416638f07ee71775cb9aeae79413b4
Hacker_Today <shekhargpt5@gmail.com>
2017-08-13 08:49:00 +0530

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,14 @@
+
+var ginfo;
+var authToken = {};
+var parentId;
+var currentId;
+var Id;
+
+
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.tabs.create({'url': 'html/memo.html'}, function(tab) {
+});
+
+});
+
